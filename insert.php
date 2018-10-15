650f69570c8e38e5a0a1245bf882b01e398edee9
Carter McGhee <cm4rn@virginia.edu>
2018-10-15 17:56:23 -0400

    testing again

---
+++ insert.php
@@ -0,0 +1,12 @@
+<?php
+	$host        = "host = ec2-184-72-234-230.compute-1.amazonaws.com";
+	$port        = "port = 5432";
+	$dbname      = "dbname = d3au1dsacafa29";
+	$credentials = "user = zbujcjxtcupcbv  password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646";
+
+	$db = pg_connect( "$host $port $dbname $credentials"  );
+
+	$query = "INSERT INTO siteUsers (email, password, name, state, address, zip, city) VALUES ('$_POST[email]','$_POST[password]', '$_POST[name]','$_POST[State]', '$_POST[address]', '$_POST[zip]', '$_POST[city]')";
+	pg_query($query);
+	echo $query;
+?>
--- signup.php
+++ signup.php
@@ -45,7 +45,7 @@
 				<h2>Sign Up</h2>
 			</header>
 			<div class="box">
-				<form name="insert" method="post" action="signup.php">
+				<form name="insert" method="post" action="insert.php">
 					<div class="row gtr-50 gtr-uniform">
 						<div class="col-6 col-12-mobilep">
 							Name
@@ -194,15 +194,3 @@
 		</script>
 </body>
 </html>
-
-<?php
-	$host        = "host = ec2-184-72-234-230.compute-1.amazonaws.com";
-	$port        = "port = 5432";
-	$dbname      = "dbname = d3au1dsacafa29";
-	$credentials = "user = zbujcjxtcupcbv  password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646";
-
-	$db = pg_connect( "$host $port $dbname $credentials"  );
-
-	$query = "INSERT INTO siteUsers VALUES ('$_POST[email]','$_POST[password]', '$_POST[name]','$_POST[State]', '$_POST[address]', '$_POST[zip]', '$_POST[city]')";
-	$result = pg_query($query); 
-?>
