9697638fb5d9ada8d6551ff7d55722dd103ef8b9
Carter McGhee <cm4rn@virginia.edu>
2018-10-15 18:27:44 -0400

    testing again 2

--- insert.php
+++ insert.php
@@ -1,11 +1,6 @@
 <?php
-	$host        = "host = ec2-184-72-234-230.compute-1.amazonaws.com";
-	$port        = "port = 5432";
-	$dbname      = "dbname = d3au1dsacafa29";
-	$credentials = "user = zbujcjxtcupcbv  password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646";
-
-	$db = pg_connect( "$host $port $dbname $credentials"  );
-	if(!$db) {
+   	$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
+   	if(!$dbconn) {
       echo "Error : Unable to open database\n";
    	} else {
       echo "Opened database successfully\n";
@@ -14,10 +9,11 @@
 	$query = "INSERT INTO siteUsers (email, password, name, state, address, zipcode, city) VALUES ('$_POST[email]','$_POST[password]', '$_POST[name]','$_POST[State]', '$_POST[address]', '$_POST[zip]', '$_POST[city]')";
 	//pg_query($db, $query);
 
-	if (pg_query($db,$query))  {
+	if (pg_query($dbconn,$query))  {
         echo "user created successfully. ";
     }
     else  {
         echo "Error creating user. ";
     }
+    echo $query;
 ?>
