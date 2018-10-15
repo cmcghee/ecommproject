fe15d3a162bbf31d2f3f9dce82d3583b47e89956
Carter McGhee <cm4rn@virginia.edu>
2018-10-15 18:15:04 -0400

    test

--- insert.php
+++ insert.php
@@ -5,8 +5,13 @@
 	$credentials = "user = zbujcjxtcupcbv  password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646";
 
 	$db = pg_connect( "$host $port $dbname $credentials"  );
+	if(!$db) {
+      echo "Error : Unable to open database\n";
+   	} else {
+      echo "Opened database successfully\n";
+   	}
 
-	$query = ($db, "INSERT INTO siteUsers (email, password, name, state, address, zip, city) VALUES ('$_POST[email]','$_POST[password]', '$_POST[name]','$_POST[State]', '$_POST[address]', '$_POST[zip]', '$_POST[city]')");
+	$query = "INSERT INTO siteUsers (email, password, name, state, address, zipcode, city) VALUES ('$_POST[email]','$_POST[password]', '$_POST[name]','$_POST[State]', '$_POST[address]', '$_POST[zip]', '$_POST[city]')";
 	pg_query($query);
 	echo $query;
 ?>
