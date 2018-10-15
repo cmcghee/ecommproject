85c240906d1d5665a985003b75621a3f4b2c98dd
Carter McGhee <cm4rn@virginia.edu>
2018-10-15 16:37:35 -0400

    added php db connection

--- GlideTech/index.php
+++ GlideTech/index.php
@@ -1,9 +1,17 @@
-<!DOCTYPE HTML>
-<!--
-	Alpha by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
+<?php
+   $host        = "host = ec2-184-72-234-230.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d3au1dsacafa29";
+   $credentials = "user = zbujcjxtcupcbv  password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+?>
+
 <html>
 	<head>
 		<title></title>
