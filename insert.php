8e8db8d7bb97949b2a07d4863db69d74406d8280
Carter McGhee <cm4rn@virginia.edu>
2018-10-15 22:27:30 -0400

    test

--- insert.php
+++ insert.php
@@ -2,9 +2,8 @@
    	$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
 
    	$hashedpassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-   	$hashed = (string) $hashedpassword;
 
-	$query = "INSERT INTO currentusers (email, password, name, state, address, zipcode, city) VALUES ('$_POST[email]', $hashed, '$_POST[name]','$_POST[State]', '$_POST[address]', '$_POST[zip]', '$_POST[city]')";
+	$query = "INSERT INTO currentusers (email, password, name, state, address, zipcode, city) VALUES ('$_POST[email]', '$hashedpassword', '$_POST[name]','$_POST[State]', '$_POST[address]', '$_POST[zip]', '$_POST[city]')";
 
 	if (pg_query($dbconn,$query))  {
         $text = "Success";
