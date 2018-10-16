8bfc1d87dcd87e14e07e52894a3cde835c6ac467
Carter McGhee <cm4rn@virginia.edu>
2018-10-15 22:05:22 -0400

    test

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<?php
-   $host        = "host = ec2-184-72-234-230.compute-1.amazonaws.com";
-   $port        = "port = 5432";
-   $dbname      = "dbname = d3au1dsacafa29";
-   $credentials = "user = zbujcjxtcupcbv  password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646";
-
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-?>
-
+<!DOCTYPE html>
 <html>
 	<head>
 		<title></title>
