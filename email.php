2bbda5b3c587606796865cde8b0abd4451668bcb
Carter McGhee <cm4rn@virginia.edu>
2018-10-29 19:53:53 -0400

    Added phpmailer/composer fixe2

--- email.php
+++ email.php
@@ -1,25 +1,31 @@
-<?php
+<?php    
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 if(isset($_POST['submit'])){
-    
-require_once('PHPMailer-master/src/class.phpmailer.php');
-require("PHPMailer-master/src/SMTP.php");
-require("PHPMailer-master/src/Exception.php");
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $message= $_POST['message'];
 
- $name = $_POST['name'];
- $email = $_POST['email'];
- $message = $_POST['message'];
- $mail = new PHPMailer\PHPMailer\PHPMailer();
- $mail->setFrom('glidetechcompany@gmail.com', 'GlideTech');
- $mail->addAddress($email);
- $mail->Subject  = 'Contact Us Submission';
- $mail->Body     = "Thank you for contacting our service!";
- if(!$mail->send()) {
-   echo 'Message was not sent.';
-   echo 'Mailer error: ' . $mail->ErrorInfo;
- } else {
-   echo 'Message has been sent.';
- }
-    
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+
+    //Recipients
+    $mail->setFrom('glidetechcompany@gmail.com', 'GlideTech');
+    $mail->addAddress($email);     // Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Contact Us Recieved';
+    $mail->Body    = 'Thanks for contacting us!';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}}
+else {
+    echo "Message Not Sent";
 }
-
-?>
