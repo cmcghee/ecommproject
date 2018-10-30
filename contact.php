cbe7f74a4007cc2c9bdf282554c9b59675e3136b
Carter McGhee <cm4rn@virginia.edu>
2018-10-29 20:12:57 -0400

    Added phpmailer/composer fixe2

--- contact.php
+++ contact.php
@@ -1,28 +1,71 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-<head>
-    <title>Contact Us</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    <h2>Contact Us</h2>
-    <form method="post" action="email.php">
-  <div class="form-group">
-    <label for="name">Name</label>
-    <input type="text" name="name" class="form-control" id="name" placeholder="Enter fullname">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="email">Enter Your Email</label>
-    <input type="email" name="email" class="form-control" id="email" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <label for="message">Write Something</label>
-    <textarea id="message" name="message" class="form-control"></textarea>
-  </div>
-  <button type="submit" name="submit" class="btn btn-primary">Send Email</button>
-</form>
-</div>
-</body>
+  <head>
+    <title>Contact - Alpha by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/contact.css" />
+  </head>
+  <body class="is-preload">
+    <div id="page-wrapper">
+
+      <!-- Header -->
+        <header id="header" class="alt">
+          <a href="index.html"></a><span class="image featured"><img src="images/logo.png" width="40%" /></span>
+          <nav id="nav">
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li>
+                <a href="#" class="icon fa-angle-down">Menu</a>
+                <ul>
+                  <li><a href="signup.html">Sign Up</a></li>
+                  <li><a href="login.html">Log In</a></li>
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                </ul>
+              <li><a href="index.html">Purchase</a></li>
+            </ul>
+          </nav>
+        </header>
+
+      <!-- Main -->
+        <section id="main" class="container medium">
+          <header>
+            <h2>Contact Us</h2>
+          </header>
+          <div class="box">
+            <form method="post" action="email.php">
+              <div class="row gtr-50 gtr-uniform">
+                <div class="col-6 col-12-mobilep">
+                  <input type="text" name="name" id="name" value="" placeholder="Name" />
+                </div>
+                <div class="col-6 col-12-mobilep">
+                  <input type="email" name="email" id="email" value="" placeholder="Email" />
+                </div>
+                <div class="col-12">
+                  <input type="text" name="subject" id="subject" value="" placeholder="Subject" />
+                </div>
+                <div class="col-12">
+                  <textarea name="message" id="message" placeholder="Enter your message" rows="6"></textarea>
+                </div>
+                <div class="col-12">
+                  <ul class="actions special">
+                    <button type="submit" name="submit" class="btn btn-default submit">Send Message</button>
+                  </ul>
+                </div>
+              </div>
+            </form>
+          </div>
+        </section>
+
+    <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/jquery.dropotron.min.js"></script>
+      <script src="assets/js/jquery.scrollex.min.js"></script>
+      <script src="assets/js/browser.min.js"></script>
+      <script src="assets/js/breakpoints.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <script src="assets/js/main.js"></script>
+
+  </body>
 </html>
--- email.php
+++ email.php
@@ -38,3 +38,54 @@ try {
 else {
     echo "Message Not Sent";
 }
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Contact - Alpha by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/contact.css" />
+  </head>
+  <body class="is-preload">
+    <div id="page-wrapper">
+
+      <!-- Header -->
+        <header id="header" class="alt">
+          <a href="index.html"></a><span class="image featured"><img src="images/logo.png" width="40%" /></span>
+          <nav id="nav">
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li>
+                <a href="#" class="icon fa-angle-down">Menu</a>
+                <ul>
+                  <li><a href="signup.html">Sign Up</a></li>
+                  <li><a href="login.html">Log In</a></li>
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                </ul>
+              <li><a href="index.html">Purchase</a></li>
+            </ul>
+          </nav>
+        </header>
+
+      <!-- Main -->
+        <section id="main" class="container medium">
+          <header>
+            <h2>We will contact you back shortly</h2>
+          </header>
+        </section>
+
+    <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/jquery.dropotron.min.js"></script>
+      <script src="assets/js/jquery.scrollex.min.js"></script>
+      <script src="assets/js/browser.min.js"></script>
+      <script src="assets/js/breakpoints.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
