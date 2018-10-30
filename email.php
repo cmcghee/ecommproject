60fe871e5afe3d0cf9c781d5acf44344ef8d6f9c
Carter McGhee <cm4rn@virginia.edu>
2018-10-29 20:08:44 -0400

    Added phpmailer/composer fixe2

--- email.php
+++ email.php
@@ -11,15 +11,24 @@ require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'glidetechcompany@gmail.com';                 // SMTP username
+    $mail->Password = 'mcghee13';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom('glidetechcompany@gmail.com', 'GlideTech');
     $mail->addAddress($email);     // Add a recipient
 
     //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Contact Us Recieved';
-    $mail->Body    = 'Thanks for contacting us!';
+    //$mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Contact Us Submission';
+    $mail->Body    = 'Thanks for the feedback!';
 
     $mail->send();
     echo 'Message has been sent';
