2830180866c25dbfe85d024509380d43c8971ea2
Carter McGhee <cm4rn@virginia.edu>
2018-11-18 20:18:52 -0500

    Fixed routing

--- login.php
+++ login.php
@@ -83,7 +83,7 @@
 		$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
 		session_start();
 		$email=$_POST['email'];
-		$password=$_POST['password'];
+		$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 		$_SESSION['login_user']=$username; 
 		$query = "SELECT email FROM currentusers WHERE email='$email' and password='$password'";
 		 if (pg_num_rows($query) != 0)
