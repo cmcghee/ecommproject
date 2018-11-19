50871bb93d05d7e3a727436626da1d621eb572f6
Bryce  Cuthriell <brycecuthriell@d-172-25-44-137.dhcp.virginia.edu>
2018-11-19 18:33:48 -0500

    edits

--- userinfo.php
+++ userinfo.php
@@ -1,4 +1,6 @@
-
+<?php
+    
+?>
 <!DOCTYPE HTML>
 <html>
   <head>
@@ -32,6 +34,7 @@
         <section id="main" class="container medium">
           <header>
             <h2>View Your Information</h2>
+
             <?php
             $dbconn = db_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
             echo 'foobar';
