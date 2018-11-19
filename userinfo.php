622d7120943513358e1425e5b331afc0616d2045
Bryce  Cuthriell <brycecuthriell@d-172-25-99-29.dhcp.virginia.edu>
2018-11-19 14:30:33 -0500

    created view info page

---
+++ userinfo.php
@@ -0,0 +1,119 @@
+<?php
+$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
+
+$query= mysql_query("SELECT * FROM `d3au1dsacafa29` WHERE `email` = '".$_SESSION['email']."' ")or die(mysql_error());
+$arr = mysql_fetch_array($query);
+$num = mysql_numrows($query);
+?>
+
+
+
+
+
+
+
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>View Your Inforamtion</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/contact.css" />
+  </head>
+  <body class="is-preload">
+    <div id="page-wrapper">
+
+      <!-- Header -->
+        <header id="header" class="alt">
+          <a href="index.php"></a><span class="image featured"><img src="images/logo.png" width="50%" /></span>
+          <nav id="nav">
+            <ul>
+              <li><a href="index.php">Home</a></li>
+              <li>
+                <a href="#" class="icon fa-angle-down">Menu</a>
+                <ul>
+                <li><a href="memberabout.html">View Profile</a></li> <!-- obviously needs to have a new link when we have it -->
+                <li><a href="memberabout.html">About Us</a></li>
+                <li><a href="membercontact.php">Contact</a></li>
+                </ul>
+                <li><a href="index.php">Logout</a></li>
+            </ul>
+          </nav>
+        </header>
+
+      <!-- Main -->
+        <section id="main" class="container medium">
+          <header>
+            <h2>View Your Information</h2>
+          </header>
+          <!-- <div class="box">
+            <form method="post" action="email.php">
+              <div class="row gtr-50 gtr-uniform">
+                <div class="col-6 col-12-mobilep">
+                  <input type="text" name="name" id="name" value="" placeholder="Name" />
+                </div>
+                <div class="col-6 col-12-mobilep">
+                  <input type="email" name="email" id="email" value="" placeholder="Email" />
+                </div>
+                <div class="col-12">
+                  <input type="text" name="subject" id="subject" value="" placeholder="Subject" />
+                </div>
+                <div class="col-12">
+                  <textarea name="message" id="message" placeholder="Enter your message" rows="6"></textarea>
+                </div>
+                <div class="col-12">
+                  <ul class="actions special">
+                    <button type="submit" name="submit" class="button">Send Message</button>
+                  </ul>
+                </div>
+              </div>
+            </form>
+          </div> -->
+        </section>
+
+    <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/jquery.dropotron.min.js"></script>
+      <script src="assets/js/jquery.scrollex.min.js"></script>
+      <script src="assets/js/browser.min.js"></script>
+      <script src="assets/js/breakpoints.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <script src="assets/js/main.js"></script>
+
+  </body>
+
+
+<?php if($num > 0){ ?>
+<table border="1" cellpadding="3">
+<tr><td colspan="2" align="center">Your Info</td></tr>
+<tr>
+ <td>Name: <?php echo $arr['name']; ?></td>
+</tr>
+
+<tr>
+ <td>Email: <?php echo $arr['email']; ?></td>
+</tr>
+
+<tr>
+ <td>Address: <?php echo $arr['address']; ?></td>
+</tr>
+
+<tr>
+ <td>City: <?php echo $arr['city']; ?></td>
+</tr>
+
+<tr>
+ <td>State: <?php echo $arr['state']; ?></td>
+</tr>
+
+<tr>
+ <td>Zipcode: <?php echo $arr['zipcode']; ?></td>
+</tr>
+
+</table>
+<?php }else{ ?>
+ User not found.
+<?php } ?>
+
+
+</html>
