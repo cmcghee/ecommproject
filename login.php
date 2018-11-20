f3a8c7562c4f148edb023c6fbe3e36e0049ad710
Bryce  Cuthriell <brycecuthriell@d-172-25-44-137.dhcp.virginia.edu>
2018-11-19 22:04:01 -0500

    redirected correct input from login to memberpage.php

--- login.php
+++ login.php
@@ -97,7 +97,7 @@
 
 
 		if (password_verify($password, $hashedpassword)){
-			echo "<script language='javascript' type='text/javascript'> location.href='index.php' </script>";
+			echo "<script language='javascript' type='text/javascript'> location.href='memberpage.php' </script>";
 		} else {
 			echo "<script type='text/javascript'>alert('Incorrect Login')</script>";
 		}
