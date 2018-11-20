c1a5462090bb4ab89e973e3d40281fc5a5cfaf5f
Bryce  Cuthriell <brycecuthriell@d-172-25-44-137.dhcp.virginia.edu>
2018-11-19 19:00:37 -0500

    edits

--- userinfo.php
+++ userinfo.php
@@ -38,7 +38,7 @@ ini_set('display_errors', 1);
             <h2>View Your Information</h2>
 
             <?php
-            $dbconn = db_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
+            $dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
             echo 'foobar';
             $query = pg_query("SELECT * FROM currentusers WHERE email='cm4rn@virginia.edu'", $dbconn);
 
