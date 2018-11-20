d10c8b1abb653fc953c410c613ab14455daf1d4d
Joe <joseph.wang@simoncomputing.com>
2018-11-19 19:25:25 -0500

    Hash

--- send.php
+++ send.php
@@ -1,10 +1,11 @@
 <?php
    	$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
-       $email = $_GET['email'];
-	$password = $_GET['password'];
+       $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
 	$query = "SELECT password, email FROM currentusers WHERE email='$email'";
 	$pass = pg_query($dbconn,$query) or die('Email not found.');
-	if(password_verify($password,$pass)){
+	if(password_verify($hashedpassword,$pass)){
 		Header("location: memberpage.php");
 	}
 	else{
