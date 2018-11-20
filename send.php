49f5d74537748f0929d2a7e9254bd61a99f8bb02
Joe <joseph.wang@simoncomputing.com>
2018-11-19 19:34:28 -0500

    no

--- send.php
+++ send.php
@@ -4,8 +4,8 @@
     $password = $_POST['password'];
     $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
     $query = "SELECT password FROM currentusers WHERE email='$email'";
-    echo($query);
-	$pass = pg_query($dbconn,$query) or die('Email not found.');
+    $pass = pg_query($dbconn,$query) or die('Email not found.');
+    echo($pass);
 	if(password_verify($hashedpassword,$pass)){
 		Header("location: memberpage.php");
 	}
