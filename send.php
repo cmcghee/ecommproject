f6844a79272d613c9d5da6932c456eff1e517735
Joe <joseph.wang@simoncomputing.com>
2018-11-19 19:03:31 -0500

    echo

--- send.php
+++ send.php
@@ -1,6 +1,7 @@
 <?php
    	$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
-	   $email = $_GET['email'];
+       $email = $_GET['email'];
+       echo $email;
 	$password = $_GET['password'];
 	$query = "SELECT email, password FROM currentusers WHERE email=$email";
 	$pass = pg_query($dbconn,$query) or die('Email not found.');
