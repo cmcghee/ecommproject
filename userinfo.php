e0808a5a7584aeef5cb5cb5e55aba7e1f07a7616
Bryce  Cuthriell <brycecuthriell@d-172-25-44-137.dhcp.virginia.edu>
2018-11-19 22:38:13 -0500

    added session  to current user

--- userinfo.php
+++ userinfo.php
@@ -42,8 +42,9 @@ ini_set('display_errors', 1);
 
             <?php
             $dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
-            $user =  $_SESSION['login_user'];
-            $query = pg_query($dbconn, "SELECT * FROM currentusers WHERE email=$user ");
+            $user = $_SESSION['login_user'];
+            echo $user;
+            $query = pg_query($dbconn, "SELECT * FROM currentusers WHERE email= $user ");
                 $row = pg_fetch_row($query);
             ?>
 
