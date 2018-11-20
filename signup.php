c9ee53c5cae8d721e50388659170d98d8b28516a
Bryce  Cuthriell <brycecuthriell@d-172-25-44-137.dhcp.virginia.edu>
2018-11-19 23:21:48 -0500

    edits

--- signup.php
+++ signup.php
@@ -290,6 +290,9 @@ $action = '';
 		<script src="assets/js/breakpoints.min.js"></script>
 		<script src="assets/js/util.js"></script>
 		<script src="assets/js/main.js"></script>
+
+
+
 </body>
 
 </html>
