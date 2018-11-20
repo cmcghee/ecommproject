bc9568ef14a3e16075f1efb22261daea22f53949
Bryce  Cuthriell <brycecuthriell@d-172-25-44-137.dhcp.virginia.edu>
2018-11-19 23:38:09 -0500

    edits

--- userinfo.php
+++ userinfo.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 ini_set('display_errors', 1);
 ?>
 <!DOCTYPE HTML>
@@ -44,7 +45,7 @@ ini_set('display_errors', 1);
             $dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
             $user = $_SESSION['login_user'];
 
-            $query = pg_query($dbconn, "SELECT * FROM currentusers WHERE email='joewang1324072@gmail.com' ");
+            $query = pg_query($dbconn, "SELECT * FROM currentusers WHERE email='$email' ");
             
                 $row = pg_fetch_row($query);
             ?>
