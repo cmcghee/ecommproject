409d741e3beea99b63fa9cb332984993c035187d
Bryce  Cuthriell <brycecuthriell@Bryces-MacBook-Air.local>
2018-11-20 19:33:35 -0500

    edits

--- logout.php
+++ logout.php
@@ -1,10 +1,20 @@
 <?php
 
-session_start();
-unset($_SESSION);
-session_destroy();
-session_write_close();
-header('Location:index.php');
-die;
+// session_start();
+// unset($_SESSION);
+// session_destroy();
+// session_write_close();
+// header('Location:index.php');
+// die;
+
+?>
+
+<?php
+
+if(isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['login_user']);
+    header('location:index.php');
+}
 
 ?>
