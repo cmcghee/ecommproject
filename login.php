9b812706a4c7c13bb0581db2fe450b3097d4cda6
Bryce  Cuthriell <brycecuthriell@Bryces-MacBook-Air.local>
2018-11-20 22:46:23 -0500

    edits

--- login.php
+++ login.php
@@ -87,7 +87,7 @@
 
 		$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
 
-		// session_start();
+		session_start();
 		$email=$_POST['email'];
 		$password = $_POST['password'];
 		$_SESSION['login_user']=$email; 
