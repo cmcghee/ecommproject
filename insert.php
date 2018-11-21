1a8e60dc27cc077eb22d7c509573be5624508194
Bryce  Cuthriell <brycecuthriell@Bryces-MacBook-Air.local>
2018-11-21 00:00:24 -0500

    edits

--- insert.php
+++ insert.php
@@ -1,4 +1,8 @@
 <?php
+
+if (isset($_POST['submit']))
+		{
+
    	$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
 
    	$hashedpassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
