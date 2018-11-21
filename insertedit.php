b4e5d074e06c6579e9fb1d222a5a6c1eb276aeb2
Joe <joseph.wang@simoncomputing.com>
2018-11-21 00:49:23 -0500

    insertedit

--- insertedit.php
+++ insertedit.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if(!($_SESSION['loggedin'] == 1)){
+	header("Location: index.php");
+}
    	$dbconn = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d3au1dsacafa29 user=zbujcjxtcupcbv password=a415fc185f58773e0af4dcf7a642a5ae27158298a6b28dbcbe3dfd4c4cb9d646");
 
    	// $hashedpassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
